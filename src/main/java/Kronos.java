fe3ceb3769c7205fe2317c23e7b3d9f184714243
ryantanzr <ryantanzr.work@gmail.com>
2025-08-14 17:54:41 +0800

    Renamed chatbot to Kronos
    Provided a greeting and exit message with dividers

---
+++ src/main/java/Kronos.java
@@ -0,0 +1,12 @@
+public class Kronos {
+    public static void main(String[] args) {
+        
+        String divider = "____________________________________________________________\n";
+        String greetingMessage = "Hello! I'm Kronos\n" + "What can I do for you?\n";
+        String exitMessage = "Bye. Hope to see you again soon!\n";
+
+        System.out.println(divider + greetingMessage + divider);
+        System.out.println(exitMessage + divider);
+        
+    }
+}
