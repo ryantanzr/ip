86005a01356dd93b5889a4499a352762f8049fca
ryantanzr <ryantanzr.work@gmail.com>
2025-08-14 23:08:15 +0800

    Added marking and unmarking functionality
    Kronos is now able to mark tasks as completed and unmark completed tasks

--- src/main/java/Kronos.java
+++ src/main/java/Kronos.java
@@ -3,7 +3,7 @@ public class Kronos {
     private static int counter = 0;
     private static final int MAX_SIZE = 100;
     private static String divider = "____________________________________________________________\n";
-    private static String[] storage = new String[MAX_SIZE];
+    private static Task[] storage = new Task[MAX_SIZE];
     
     /**
      * Handles the user's entered message
@@ -14,8 +14,10 @@ public class Kronos {
     public static boolean handleRequest(String request) {
 
         boolean shouldExit = false;
+        String[] requestComponents = request.split(" ");
+        String keyword = requestComponents[0];
 
-        if (request.equals( "bye")) {
+        if (keyword.equals( "bye")) {
 
             // Toggle shouldExit to true
             shouldExit = true;
@@ -24,19 +26,70 @@ public class Kronos {
             String exitMessage = "Till next time...";
             System.out.println(divider + exitMessage + "\n" + divider);
         
-        } else if (request.equals("list")) {
+        } else if (keyword.equals("list")) {
             
-            String storedText = divider;
+            String listMessage = "Here are the tasks I've helped you store: \n";
+            String storedText = divider + listMessage;
+
             // Start a for loop to print out the stored text
             for (int index = 0; index < counter; ++index) {
-                storedText += String.format("%d. %s \n", index + 1, storage[index]);
+
+                String taskDescription = storage[index].getDescription();
+                String taskStatus = storage[index].getCompletionStatus();
+
+                storedText += String.format("%d.[%s] %s \n", index + 1, taskStatus, taskDescription);
             }
+            System.out.println(storedText + divider);
+
+        } else if (keyword.equals("mark")) {
+            
+            String markMessage = "Marking that task as completed: \n";
+            String storedText = divider + markMessage;
+
+            // Extract the task number
+            Integer taskNumber = Integer.parseInt(requestComponents[1]) - 1;
+
+            // Mark task as completed
+            storage[taskNumber].setCompletionStatus(true);
+
+            // Extract task details
+            String taskDescription = storage[taskNumber].getDescription();
+            String taskStatus = storage[taskNumber].getCompletionStatus();
+
+            // Format the string
+            storedText += String.format("  [%s] %s \n", taskStatus, taskDescription);
+            
+            // Print out the full response
+            System.out.println(storedText + divider);
+
+        } else if (keyword.equals("unmark")) {
+            
+            String unmarkMessage = "Marking that task as incomplete: \n";
+            String storedText = divider + unmarkMessage;
+
+            // Extract the task number
+            Integer taskNumber = Integer.parseInt(requestComponents[1]) - 1;
+
+            // Mark task as imcomplete
+            storage[taskNumber].setCompletionStatus(false);
+
+            // Extract task details
+            String taskDescription = storage[taskNumber].getDescription();
+            String taskStatus = storage[taskNumber].getCompletionStatus();
+
+            // Format the string
+            storedText += String.format("  [%s] %s \n", taskStatus, taskDescription);
+            
+            // Print out the full response
             System.out.println(storedText + divider);
 
         } else {
 
-            //Store the text and increment the counter
-            storage[counter++] = request;
+            // Assign the content to a variable with a more fitting name
+            String taskDescription = keyword;
+
+            // Store the text and increment the counter
+            storage[counter++] = new Task(taskDescription);
             System.out.println(divider + "added: "+ request + "\n" + divider);
         
         }
