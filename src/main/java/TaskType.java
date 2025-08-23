e215a5b775ca6fcca00003abc2ae0d5fdc598474
ryantanzr <ryantanzr.work@gmail.com>
2025-08-23 23:41:50 +0800

    Introduced initialisation function for TaskType

--- src/main/java/TaskType.java
+++ src/main/java/TaskType.java
@@ -1,5 +1,19 @@
 public enum TaskType {
     TODO,
     DEADLINE,
-    EVENT
+    EVENT;
+
+    public static TaskType fromString(String keyword) {
+
+        if (keyword.equals("todo")) {
+            return TODO;
+        } else if (keyword.equals("deadline")) {
+            return DEADLINE;
+        } else if (keyword.equals("event")) {
+            return EVENT;
+        } else {
+            throw new IllegalArgumentException("Sir what do you mean by: " + keyword + "?");
+        }
+
+    }
 }
