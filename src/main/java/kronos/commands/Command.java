ee647e038657db0910734f078d8c8f9c79327fe2
ryantanzr <ryantanzr.work@gmail.com>
2025-09-02 16:05:41 +0800

    Add header comment to Command class

--- src/main/java/kronos/commands/Command.java
+++ src/main/java/kronos/commands/Command.java
@@ -1,9 +1,16 @@
 package kronos.commands;
+
 import kronos.tasklist.TaskList;
 
 /**
  * Represents an abstract command that Kronos can execute.
  */
 public interface Command {
+
+    /**
+     * Executes logic related to handling tasks.
+     * @param taskList The task list to operate on.
+     * @return A message indicating the result of the command execution.
+     */
     public String execute(TaskList taskList);
 }
