89d6866d49f01102c0846307804231b166cdc6d0
ryantanzr <ryantanzr.work@gmail.com>
2025-09-17 21:40:46 +0800

    Correct exception thrown by TagCommand

--- src/main/java/kronos/commands/TagCommand.java
+++ src/main/java/kronos/commands/TagCommand.java
@@ -2,6 +2,7 @@ package kronos.commands;
 
 import java.util.Arrays;
 
+import kronos.exceptions.KronosException;
 import kronos.tasklist.TaskList;
 import kronos.tasks.Task;
 
@@ -30,16 +31,15 @@ public class TagCommand implements Command {
      * Validates the tags before adding them to the task.
      * @param currentTagCount The current number of tags on the task.
      * @param tags The array of tags to validate.
-     * @throws IllegalArgumentException If any tag is invalid.
+     * @throws KronosException If any tag is invalid.
      */
-    private void validateTags(int currentTagCount, String... tags) throws IllegalArgumentException {
+    private void validateTags(int currentTagCount, String... tags) throws KronosException {
         if (currentTagCount + tags.length > MAX_TAGS) {
-            throw new IllegalArgumentException("Sir, a task can have a maximum of " + MAX_TAGS + " tags.");
+            throw new KronosException("A task can have a maximum of " + MAX_TAGS + " tags!");
         }
         for (String tag : tags) {
             if (!tag.matches(TAG_PATTERN)) {
-                throw new IllegalArgumentException("Tag '" + tag + "' contains invalid characters. "
-                        + "Only alphanumeric characters and underscores are allowed.");
+                throw new KronosException("Only alphanumeric characters and underscores are allowed!");
             }
         }
     }
@@ -48,9 +48,10 @@ public class TagCommand implements Command {
      * Adds tags to a task.
      * @param taskList The list of tasks to modify.
      * @return A formatted string showing the new tags.
+     * @throws KronosException If the task number is invalid or tags are invalid.
      */
     @Override
-    public String execute(TaskList taskList) {
+    public String execute(TaskList taskList) throws KronosException {
 
         String responseString = "Tagged the task with: ";
         int extractionStartIndex = 2;
